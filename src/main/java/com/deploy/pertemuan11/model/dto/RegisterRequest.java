028eceae0b16026bee06e5ffb3f57df83463c30a
wpiskaa <hafizzkurniawan19@gmail.com>
2026-05-21 13:49:28 +0700

    feat: add User and Profile entity with OneToOne JPA relationship

---
+++ src/main/java/com/deploy/pertemuan11/model/dto/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.deploy.pertemuan11.model.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String nama;
+    private String alamat;
+}
