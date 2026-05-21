b28ea0d13097557b11d6cd8ab197ad9a29ed0d26
wpiskaa <hafizzkurniawan19@gmail.com>
2026-05-21 13:49:40 +0700

    feat: add AuthService with register() and getLoggedInUser() methods

---
+++ src/main/java/com/deploy/pertemuan11/service/AuthService.java
@@ -0,0 +1,50 @@
+package com.deploy.pertemuan11.service;
+
+import com.deploy.pertemuan11.model.Profile;
+import com.deploy.pertemuan11.model.User;
+import com.deploy.pertemuan11.model.dto.RegisterRequest;
+import com.deploy.pertemuan11.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register(RegisterRequest request) {
+        User user = User.builder()
+                .id(UUID.randomUUID().toString())
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        Profile profile = Profile.builder()
+                .id(UUID.randomUUID().toString())
+                .nama(request.getNama())
+                .alamat(request.getAlamat())
+                .user(user)
+                .build();
+
+        user.setProfile(profile);
+
+        userRepository.save(user);
+    }
+
+    public User getLoggedInUser() {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User tidak ditemukan"));
+    }
+}
