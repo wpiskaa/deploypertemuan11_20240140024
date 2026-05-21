4f2be62df9a0ddd4e9c67a6e8832bf3ea1b0e4e8
wpiskaa <hafizzkurniawan19@gmail.com>
2026-05-21 13:49:34 +0700

    feat: add UserRepository and ProfileRepository extending JpaRepository

---
+++ src/main/java/com/deploy/pertemuan11/repository/ProfileRepository.java
@@ -0,0 +1,9 @@
+package com.deploy.pertemuan11.repository;
+
+import com.deploy.pertemuan11.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, String> {
+}
